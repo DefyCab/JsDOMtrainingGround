f4d0dfc3d9042c40e909c758bec6677b3261e2a1
defycab <love.bellinder@gmail.com>
2024-09-09 10:27:28 +0200

    innerhtml

--- functions.js
+++ functions.js
@@ -1,6 +1,12 @@
-myfunctions = {
-  getCompanyName: function (name) {
-    return `${name}`
-  },
+function getCompanyName(name) {
+  return `${name}`
 }
+
+const companyName = getCompanyName("Salt")
+const companyName2 = getCompanyName("S A L T")
+
+document.getElementById("companyName").innerHTML = companyName
+document.getElementsByTagName("div")[2].innerHTML = companyName2;
+document.getElementsByTagName("div")[1].innerHTML = "Jag heter Love"
+
 console.log("salt> Functions loaded")
