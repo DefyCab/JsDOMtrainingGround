cb095c453682110c9593e560f72270ce5f1385fa
defycab <love.bellinder@gmail.com>
2024-09-18 10:14:25 +0200

    queryselector

--- functions.js
+++ functions.js
@@ -2,10 +2,32 @@ function getCompanyName(name) {
   return `${name}`
 }
 
+myFunctions = {
+  getCompanyName2: (name) => {
+    return `${name}`
+  },
+};
+
 const companyName = getCompanyName("Salt")
 const companyName2 = getCompanyName("S A L T")
 
-document.getElementById("companyName").innerHTML = companyName
+const boldNodes = document.getElementsByClassName("bold");
+
+for (let i = 0; i < boldNodes.length; i++) {
+  boldNodes[i].innerHTML += " ... and bold";
+}
+
+const companyNameElement = document.querySelector("#companyName");
+companyNameElement.innerHTML =
+  companyName + " set using a query selector by id";
+
+const boldNodesQuerySelected = document.querySelectorAll(".bold");
+for (let i = 0; i < boldNodes.length; i++) {
+  boldNodes[i].innerHTML += " ... and bold selected with query";
+}
+
+
+document.getElementById("companyName1").innerHTML = companyName
 document.getElementsByTagName("div")[2].innerHTML = companyName2;
 document.getElementsByTagName("div")[1].innerHTML = "Jag heter Love"
 
