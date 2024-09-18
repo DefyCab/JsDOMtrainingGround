f18ba2e149b307130935d8b3eb53305c395b840a
defycab <love.bellinder@gmail.com>
2024-09-18 22:57:03 +0200

    trying the DOMcontentloader

--- script.js
+++ script.js
@@ -41,4 +41,9 @@ function addStyleBasedOfContent(element, content) {
     }
   }
 
-addResultStyling()
+  document.addEventListener("DOMContentLoaded", () => {
+    console.log("Document is loaded, y'all");
+    // addResultStyling();
+  });
+
+// addResultStyling()
