7be4cd9e75e7fc63b60b5efb3ebcea2674c54c6a
defycab <love.bellinder@gmail.com>
2024-09-18 21:01:23 +0200

    changing class function works

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const divs = document.querySelectorAll(".mainResult")
 
-
 console.log(divs)
 
 function checkIfNegative(nodelist) {
-    for(let i = 0; i < nodelist.length; i++ ) {
-        if(nodelist[i].textContent < 0 ) {
-            nodelist[i].classList.add("negativeResult")
-        } 
-        nodelist[i].classList.add("positiveResult")
+  for (let i = 0; i < nodelist.length; i++) {
+    if (Number(nodelist[i].textContent) < 0) {
+      nodelist[i].classList.remove("mainResult")
+      nodelist[i].classList.add("negativeResult")
+    } else if (Number(nodelist[i].textContent) > 0) {
+      nodelist[i].classList.remove("mainResult")
+      nodelist[i].classList.add("positiveResult")
+    } else {
+      nodelist[i].classList.remove("mainResult")
+      nodelist[i].classList.add("notANumber")
     }
+  }
 }
 
 checkIfNegative(divs)
