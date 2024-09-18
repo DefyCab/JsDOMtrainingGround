9d2b9ab83858beb1d2d547946c52357088a3dfed
defycab <love.bellinder@gmail.com>
2024-09-18 10:31:12 +0200

    append and createelement

--- functions.js
+++ functions.js
@@ -6,29 +6,48 @@ myFunctions = {
   getCompanyName2: (name) => {
     return `${name}`
   },
-};
+}
+
+const items = ["Love", "Erik", "Sven"]
+
+function addListInMain(numberOfListItems) {
+  const ulElement = document.createElement("ul")
+  console.log(ulElement)
+
+  for (let i = 0; i < numberOfListItems; i++) {
+    const liItem = document.createElement("li")
+    //textContent is like writing between ><
+    liItem.textContent = `Item #${i + 1}`
+
+    ulElement.appendChild(liItem)
+  }
+
+  // . means class otherwise main element
+  const main = document.querySelector(".main")
+  main.appendChild(ulElement)
+}
+
+addListInMain(10)
 
 const companyName = getCompanyName("Salt")
 const companyName2 = getCompanyName("S A L T")
 
-const boldNodes = document.getElementsByClassName("bold");
+const boldNodes = document.getElementsByClassName("bold")
 
 for (let i = 0; i < boldNodes.length; i++) {
-  boldNodes[i].innerHTML += " ... and bold";
+  boldNodes[i].innerHTML += " ... and bold"
 }
 
-const companyNameElement = document.querySelector("#companyName");
-companyNameElement.innerHTML =
-  companyName + " set using a query selector by id";
+const companyNameElement = document.querySelector("#companyName")
+companyNameElement.innerHTML = companyName + " set using a query selector by id"
 
-const boldNodesQuerySelected = document.querySelectorAll(".bold");
+const boldNodesQuerySelected = document.querySelectorAll(".bold")
 for (let i = 0; i < boldNodes.length; i++) {
-  boldNodes[i].innerHTML += " ... and bold selected with query";
+  boldNodes[i].innerHTML += " ... and bold selected with query"
 }
-
 
 document.getElementById("companyName1").innerHTML = companyName
-document.getElementsByTagName("div")[2].innerHTML = companyName2;
+document.getElementsByTagName("div")[2].innerHTML = companyName2
 document.getElementsByTagName("div")[1].innerHTML = "Jag heter Love"
 
 console.log("salt> Functions loaded")
