3b49364d51cf6d6338b3075a171310ca7bbe6e75
defycab <love.bellinder@gmail.com>
2024-09-18 22:15:46 +0200

    solution with two functions cleaner

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
-const divs = document.querySelectorAll(".mainResult")
+// const divs = document.querySelectorAll(".mainResult")
 
-console.log(divs)
+// function checkIfNegative(nodelist) {
+//   for (let i = 0; i < nodelist.length; i++) {
+//     if (Number(nodelist[i].textContent) < 0) {
+//       nodelist[i].classList.remove("mainResult")
+//       nodelist[i].classList.add("negativeResult")
+//     } else if (Number(nodelist[i].textContent) > 0) {
+//       nodelist[i].classList.remove("mainResult")
+//       nodelist[i].classList.add("positiveResult")
+//     } else {
+//       nodelist[i].classList.remove("mainResult")
+//       nodelist[i].classList.add("notANumber")
+//     }
+//   }
+// }
 
-function checkIfNegative(nodelist) {
-  for (let i = 0; i < nodelist.length; i++) {
-    if (Number(nodelist[i].textContent) < 0) {
-      nodelist[i].classList.remove("mainResult")
-      nodelist[i].classList.add("negativeResult")
-    } else if (Number(nodelist[i].textContent) > 0) {
-      nodelist[i].classList.remove("mainResult")
-      nodelist[i].classList.add("positiveResult")
-    } else {
-      nodelist[i].classList.remove("mainResult")
-      nodelist[i].classList.add("notANumber")
+// checkIfNegative(divs)
+
+function addStyleBasedOfContent(element, content) {
+    const convertedNumber = Number(content);
+  
+    if (Number.isNaN(convertedNumber)) {
+      // return stoppar funktionen
+      return;
+    }
+    if (convertedNumber < 0) {
+      element.classList.add("negativeResult");
+      return;
+    }
+    element.classList.add("positiveResult");
+  }
+
+  function addResultStyling() {
+    const mainResultElements = document.querySelectorAll(".mainResult");
+  
+    for (let i = 0; i < mainResultElements.length; i++) {
+      const content = mainResultElements[i].textContent;
+  
+      addStyleBasedOfContent(mainResultElements[i], content);
     }
   }
-}
 
-checkIfNegative(divs)
+addResultStyling()
