044d21b891635cec0fd45581329dbf46366cb8ed
defycab <love.bellinder@gmail.com>
2024-09-19 20:21:34 +0200

    second button to invert color by event listener

--- script.js
+++ script.js
@@ -1,49 +1,55 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Document is loaded, y'all");
-    addResultStyling();
-  });
+  console.log("Document is loaded, y'all")
+  addResultStyling()
+})
+
+const btn = document.getElementById("btnInversion")
+btn.addEventListener("click", () => {
+  invertColorsOfResults()
+})
 
 function addStyleBasedOfContent(element, content) {
-    const convertedNumber = Number(content);
-    
-    if (Number.isNaN(convertedNumber)) {
-      // return stoppar funktionen
-      return;
-    }
-    if (convertedNumber < 0) {
-      element.classList.add("negativeResult");
-      return;
-    }
-    element.classList.add("positiveResult");
+  const convertedNumber = Number(content)
+
+  if (Number.isNaN(convertedNumber)) {
+    // return stoppar funktionen
+    return
   }
-  
-  function addResultStyling() {
-    const mainResultElements = document.querySelectorAll(".mainResult");
-    
-    for (let i = 0; i < mainResultElements.length; i++) {
-      const content = mainResultElements[i].textContent;
-  
-      addStyleBasedOfContent(mainResultElements[i], content);
+  if (convertedNumber < 0) {
+    element.classList.add("negativeResult")
+    return
+  }
+  element.classList.add("positiveResult")
+}
+
+function addResultStyling() {
+  const mainResultElements = document.querySelectorAll(".mainResult")
+
+  for (let i = 0; i < mainResultElements.length; i++) {
+    const content = mainResultElements[i].textContent
+
+    addStyleBasedOfContent(mainResultElements[i], content)
+  }
+}
+
+function invertColorsOfResults() {
+  const mainResultElements = document.querySelectorAll(".mainResult")
+
+  for (let i = 0; i < mainResultElements.length; i++) {
+    const element = mainResultElements[i]
+    console.log(element.textContent)
+
+    if (element.classList.contains("positiveResult")) {
+      element.classList.remove("positiveResult")
+      element.classList.add("negativeResult")
+      continue
+    }
+
+    if (element.classList.contains("negativeResult")) {
+      element.classList.remove("negativeResult")
+      element.classList.add("positiveResult")
+      continue
     }
   }
-  
-  function invertColorsOfResults() {
-    const mainResultElements = document.querySelectorAll(".mainResult");
-  
-    for (let i = 0; i < mainResultElements.length; i++) {
-      const element = mainResultElements[i];
-      console.log(element.textContent);
-  
-      if (element.classList.contains("positiveResult")) {
-        element.classList.remove("positiveResult");
-        element.classList.add("negativeResult");
-        continue;
-      }
-  
-      if (element.classList.contains("negativeResult")) {
-        element.classList.remove("negativeResult");
-        element.classList.add("positiveResult");
-        continue;
-      }
-    }
-  }
+}
+
